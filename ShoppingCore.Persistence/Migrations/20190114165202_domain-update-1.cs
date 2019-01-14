2c2d1dc3a1cc9550f2d1ba73d9788df874ebcf13
Chaitra <chaitradangat084@gmail.com>
2019-01-14 22:23:16 +0530

    migration for product category property in product entity

---
+++ ShoppingCore.Persistence/Migrations/20190114165202_domain-update-1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingCore.Persistence.Migrations
+{
+    public partial class domainupdate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "ProductCategories",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategories_ProductId",
+                table: "ProductCategories",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCategories_Products_ProductId",
+                table: "ProductCategories",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCategories_Products_ProductId",
+                table: "ProductCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductCategories_ProductId",
+                table: "ProductCategories");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "ProductCategories");
+        }
+    }
+}
