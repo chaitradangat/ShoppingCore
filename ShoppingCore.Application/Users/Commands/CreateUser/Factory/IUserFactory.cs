318c62a8421946eff83a7aefb0c85e249f81e0bd
Chaitra <chaitradangat084@gmail.com>
2019-01-20 15:46:56 +0530

    UserFactory for CreateUserCommand

---
+++ ShoppingCore.Application/Users/Commands/CreateUser/Factory/IUserFactory.cs
@@ -0,0 +1,9 @@
+﻿using ShoppingCore.Domain.Users;
+
+namespace ShoppingCore.Application.Users.Commands.CreateUser.Factory
+{
+    public interface IUserFactory
+    {
+        User Create(string username,string password);
+    }
+}
---
+++ ShoppingCore.Application/Users/Commands/CreateUser/Factory/UserFactory.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingCore.Domain.Users;
+
+namespace ShoppingCore.Application.Users.Commands.CreateUser.Factory
+{
+    public class UserFactory : IUserFactory
+    {
+        public User Create(string username, string password)
+        {
+            User u = new User();
+            u.UserName = username;
+            u.Password = password;
+            return u;
+        }
+    }
+}
