964ad54f2b0d300ad0c58001940510cc71407281
Chaitra <chaitradangat084@gmail.com>
2019-01-20 15:14:42 +0530

    IDatabaseService interface for implementation by the persistence layer

---
+++ ShoppingCore.Application/Interfaces/IDatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using ShoppingCore.Domain.Customers;
+using ShoppingCore.Domain.Products;
+using ShoppingCore.Domain.Sellers;
+using ShoppingCore.Domain.Users;
+using ShoppingCore.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCore.Application.Interfaces
+{
+    public interface IDatabaseService
+    {
+        DbSet<Address> Addresses { get; set; }
+
+        DbSet<Customer> Customers { get; set; }
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<ProductCategory> ProductCategories { get; set; }
+
+        DbSet<Seller> Sellers { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+    }
+}
