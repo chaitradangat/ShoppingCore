a958e4657e8496539744ec5652388e949fa59795
Chaitra <chaitradangat084@gmail.com>
2019-01-20 15:49:37 +0530

    Create User CQRS command

--- ShoppingCore.Application/Users/Commands/CreateUser/CreateUserCommand.cs
+++ ShoppingCore.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
+using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Application.Users.Commands.CreateUser.Factory;
+
 namespace ShoppingCore.Application.Users.Commands.CreateUser
 {
     class CreateUserCommand :ICreateUserCommand
     {
+        private readonly IDatabaseService _database;
+        private readonly IUserFactory _factory;
+
+
+        public CreateUserCommand(IDatabaseService database,IUserFactory factory)
+        {
+            _database = database;
+            _factory = factory;
+        }
+
         public void Execute(CreateUserModel model)
         {
-
+            var user = _factory.Create(model.UserName, model.Password);
+            _database.Users.Add(user);
+            _database.Save();
         }
     }
 }
