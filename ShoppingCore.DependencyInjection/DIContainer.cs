5f5e857030db36c39901956ddd18f966e13a5c3b
Chaitra <chaitradangat084@gmail.com>
2019-01-20 23:21:06 +0530

    Project to act as the DI container for entire application

---
+++ ShoppingCore.DependencyInjection/DIContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Application.Users.Commands.CreateUser;
+using ShoppingCore.Application.Users.Commands.CreateUser.Factory;
+using ShoppingCore.Persistence;
+
+namespace ShoppingCore.DependencyInjection
+{
+    public class DIContainer
+    {
+        public static ServiceProvider Serviceprovider { get; private set; }
+
+        public static void InjectDependencies()
+        {
+            Serviceprovider = new ServiceCollection()
+            .AddDbContext<ShoppingCoreDbContext>()
+            .AddSingleton<IDatabaseService, ShoppingCoreDbContext>()
+            .AddSingleton<IUserFactory, UserFactory>()
+            .AddSingleton<ICreateUserCommand, CreateUserCommand>()
+            .BuildServiceProvider();
+        }
+    }
+}
