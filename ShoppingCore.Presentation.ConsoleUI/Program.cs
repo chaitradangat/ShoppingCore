8a6b750c4391089ee857360ae03ea69b1fff6d53
Chaitra <chaitradangat084@gmail.com>
2019-01-20 15:54:54 +0530

    Initial Console UI for quick testing and fun ;)

---
+++ ShoppingCore.Presentation.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingCore.Presentation.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
