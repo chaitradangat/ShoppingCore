58a791681bb8d309eb99b5a13fd47b51ae3486c3
Chaitra <chaitradangat084@gmail.com>
2019-02-14 07:10:46 +0530

    Method added to force application model to implement morphing to domain model

--- ShoppingCore.Application/Interfaces/IAppModel.cs
+++ ShoppingCore.Application/Interfaces/IAppModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShoppingCore.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,6 @@ namespace ShoppingCore.Application.Interfaces
 {
     public interface IAppModel
     {
+        IEntity MorphApplicationModel();
     }
 }
