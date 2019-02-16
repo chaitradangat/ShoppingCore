f04d23207b16052d77fadad476f19a13b4c22391
Chaitra <chaitradangat084@gmail.com>
2019-02-16 23:52:27 +0530

    testing code on console..

--- ShoppingCore.Presentation.ConsoleUI/Program.cs
+++ ShoppingCore.Presentation.ConsoleUI/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +13,9 @@ using ShoppingCore.Application.Users.Commands.CreateUser.Factory;
 using ShoppingCore.Application.Interfaces;
 using ShoppingCore.Persistence;
 using Microsoft.EntityFrameworkCore.Design;
+
+//using Microsoft.EntityFrameworkCore;
+
 using ShoppingCore.Application.Customers.Commands.CreateCustomer;
 using ShoppingCore.Application.ApplicationModels;
 using ShoppingCore.Domain.Interfaces;
@@ -21,29 +28,41 @@ namespace ShoppingCore.Presentation.ConsoleUI
         {
             DIContainer.InjectDependencies();
 
+            #region -Create User Command Test-
             //ICreateUserCommand command = DIContainer.Serviceprovider.GetService<ICreateUserCommand>();
             //command.Execute(new CreateUserModel() { UserName="xxx", Password = "zzz" });
+            #endregion
 
+            #region -Create Customer Command Test-
+            //ICreateCustomerCommand command = DIContainer.Serviceprovider.GetService<ICreateCustomerCommand>();
+            //CustomerModel customerModel = new CustomerModel(DIContainer.Serviceprovider.GetService<IDomainFactory>())
+            //{
+            //    Addresses = new System.Collections.Generic.List<AddressModel>(),
+            //    AutheticationType = Domain.Common.AutheticationType.AppDatabase,
+            //    DateOfBirth = DateTime.Now,
+            //    FirstName = "FirstName test",
+            //    Gender = "Male",
+            //    LastName = "LastName test",
+            //    MiddleName = "MiddleName test",
+            //    Password = "psswrd",
+            //    UserName = "UserName test",
+            //    UserRole = Domain.Common.UserRole.Customer
+            //};
+            //command.Execute(customerModel);
+            #endregion
 
-            ICreateCustomerCommand command = DIContainer.Serviceprovider.GetService<ICreateCustomerCommand>();
+            #region -Loading related Entities-
+            //loading related entities
 
-            CustomerModel customerModel = new CustomerModel(DIContainer.Serviceprovider.GetService<IDomainFactory>())
-            {
-                Addresses = new System.Collections.Generic.List<AddressModel>(),
-                AutheticationType = Domain.Common.AutheticationType.AppDatabase,
-                DateOfBirth = DateTime.Now,
-                FirstName = "FirstName test",
-                Gender = "Male",
-                LastName = "LastName test",
-                MiddleName = "MiddleName test",
-                Password = "psswrd",
-                UserName = "UserName test",
-                UserRole = Domain.Common.UserRole.Customer
-            };
+            var database = DIContainer.Serviceprovider.GetService<IDatabaseService>();
 
-            command.Execute(customerModel);
+            var customersWithoutInclude = database.Customers.Where(c => c.FirstName.Contains("test")).ToList();
 
-
+            
+            //var customers = database.Customers.Include(c => c.User)
+            //                .Where(c => c.FirstName.Contains("test"))
+            //                .ToList();
+            #endregion
 
             Console.WriteLine("Hello World!");
             Console.ReadLine();
