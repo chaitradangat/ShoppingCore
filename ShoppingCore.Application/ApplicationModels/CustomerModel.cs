a28182e7d2098a58f5c9097a30a3832376cb45e6
Chaitra <chaitradangat084@gmail.com>
2019-02-16 22:52:16 +0530

    Changes in application  customermodel to initiate user and customer domain type simultaneously.

--- ShoppingCore.Application/ApplicationModels/CustomerModel.cs
+++ ShoppingCore.Application/ApplicationModels/CustomerModel.cs
@@ -1,8 +1,8 @@
-﻿
-using ShoppingCore.Application.Interfaces;
+﻿using ShoppingCore.Application.Interfaces;
 using ShoppingCore.Domain.Common;
 using ShoppingCore.Domain.Customers;
 using ShoppingCore.Domain.Interfaces;
+using ShoppingCore.Domain.Users;
 using System;
 using System.Collections.Generic;
 
@@ -10,6 +10,9 @@ namespace ShoppingCore.Application.ApplicationModels
 {
     public class CustomerModel : IAppModel
     {
+        private readonly IDomainFactory _factory;
+
+        #region -Properties required for new customer creation-
         public int CustomerID { get; set; }
 
         public int UserID { get; set; }
@@ -25,8 +28,22 @@ namespace ShoppingCore.Application.ApplicationModels
         public DateTime? DateOfBirth { get; set; }
 
         public List<AddressModel> Addresses { get; set; }
+        #endregion
 
-        private readonly IDomainFactory _factory;
+        #region -Properties required for new user creation-
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsAutheticated { get; set; }
+
+        public AutheticationType AutheticationType { get; set; }
+
+        public UserRole UserRole { get; set; }
+
+        #endregion
+
 
         public CustomerModel(IDomainFactory factory)
         {
@@ -35,33 +52,34 @@ namespace ShoppingCore.Application.ApplicationModels
             Addresses = new List<AddressModel>();
         }
 
+        //this method is violating loose coupling..finding better ways to do this
         public IEntity MorphAppModel()
         {
-            return null;
-        }
+            var user = (User)_factory.GetEntity<IUser>();
+            user.UserName = UserName;
+            user.Password = Password;
+            user.UserRole = UserRole;
+            user.AutheticationType = AutheticationType;
 
-        //this method is violating loose coupling..finding better ways to do this
-        public Customer MorphAppModel(IUser user)
-        {
-            var customer = (ICustomer)_factory.GetEntity<ICustomer>();
 
-            customer.UserID = user.UserID;
+            var customer = (Customer)_factory.GetEntity<ICustomer>();
+
+            customer.DateOfBirth = DateOfBirth;
 
             customer.FirstName = FirstName;
+
+            customer.MiddleName = MiddleName;
 
             customer.LastName = LastName;
 
             customer.Gender = Gender;
 
-            customer.MiddleName = MiddleName;
+            customer.User = user;
 
-            customer.DateOfBirth = DateOfBirth;
-
-            customer.Addresses = new List<Address>();
 
             foreach (var address in Addresses)
             {
-                var a = (IAddress)_factory.GetEntity<IAddress>();
+                var a = (Address)_factory.GetEntity<IAddress>();
 
                 a.AddressLine1 = address.AddressLine1;
                 a.AddressLine2 = address.AddressLine2;
@@ -75,12 +93,19 @@ namespace ShoppingCore.Application.ApplicationModels
                 a.LandMark = address.LandMark;
                 a.PinCode = address.PinCode;
                 a.Product = null;
-                a.Customer = (Customer)customer;
+                a.Customer = customer;
 
-                customer.Addresses.Add((Address)a);
+                customer.Addresses.Add(a);
             }
 
-            return (Customer)customer;
+
+
+
+
+
+
+            return customer;
+
         }
 
     }
