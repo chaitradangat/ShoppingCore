b6fcb90bdf445ca48cb17cee87084893055b82c0
Chaitra <chaitradangat084@gmail.com>
2019-02-17 23:40:03 +0530

    extension methods for loading related data :)

---
+++ ShoppingCore.Persistence/Configurations/RelatedDomainLoader.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingCore.Domain.Customers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore.Query;
+
+using ShoppingCore.Domain.Users;
+using ShoppingCore.Domain.Common;
+
+
+
+namespace ShoppingCore.Persistence.Configurations
+{
+    public static class RelatedDomainLoader
+    {
+        public static IIncludableQueryable<Customer,List<Address>> LoadRelatedEntities(this DbSet<Customer> customers)
+        {
+            return customers.Include(c => c.User).Include(c => c.Addresses);
+        }
+
+
+
+
+    }
+}
