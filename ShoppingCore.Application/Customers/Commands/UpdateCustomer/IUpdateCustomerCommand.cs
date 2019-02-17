53601ad24b7f5830269719f5a3d91cd3b7493fdb
Chaitra <chaitradangat084@gmail.com>
2019-02-17 23:39:13 +0530

    added new cqrs command for update customer

---
+++ ShoppingCore.Application/Customers/Commands/UpdateCustomer/IUpdateCustomerCommand.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCore.Application.ApplicationModels;
+using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Application.Customers.Commands.UpdateCustomer
+{
+    public interface IUpdateCustomerCommand
+    {
+        IAppModel Execute(CustomerModel customerModel);
+    }
+}
---
+++ ShoppingCore.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Application.ApplicationModels;
+using ShoppingCore.Domain.Customers;
+using ShoppingCore.Domain.Common;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ShoppingCore.Application.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommand : IUpdateCustomerCommand
+    {
+        protected readonly IDatabaseService _database;
+
+        public UpdateCustomerCommand(IDatabaseService database)
+        {
+            _database = database;
+        }
+
+        public IAppModel Execute(CustomerModel customerModel)
+        {
+            //assign the entity
+            customerModel._entity = _database.Customers.Find(customerModel.CustomerID);
+
+            customerModel.ConvertToDomainModel();
+
+            _database.Customers.Update(customerModel._entity);
+
+            _database.Save();
+
+            return customerModel;
+        }
+
+    }
+}
