c6c1552e10cad88cd6cd7780e75746bf6782ab64
Chaitra <chaitradangat084@gmail.com>
2019-02-24 20:08:05 +0530

    efcore migrations for virtual properties

---
+++ Providers/ShoppingCore.Provider.EfCore/Migrations/20190224143549_virtual-properties.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingCore.Provider.EfCore.Migrations
+{
+    public partial class virtualproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
