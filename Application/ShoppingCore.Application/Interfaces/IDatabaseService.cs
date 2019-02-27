77b6e8e3044e145f143cc224dd4b05d222f3b106
Chaitra <chaitradangat084@gmail.com>
2019-02-27 08:28:43 +0530

    replacing IDatabaseService with IPersistence<IEntity>... this will create breaking changes

--- Application/ShoppingCore.Application/Interfaces/IDatabaseService.cs
+++ Application/ShoppingCore.Application/Interfaces/IDatabaseService.cs
@@ -1,35 +1,35 @@
-﻿using ShoppingCore.Domain.Customers;
-using ShoppingCore.Domain.Products;
-using ShoppingCore.Domain.Sellers;
-using ShoppingCore.Domain.Users;
-using ShoppingCore.Domain.Common;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Linq.Expressions;
-using System.Linq;
+﻿//using ShoppingCore.Domain.Customers;
+//using ShoppingCore.Domain.Products;
+//using ShoppingCore.Domain.Sellers;
+//using ShoppingCore.Domain.Users;
+//using ShoppingCore.Domain.Common;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Linq.Expressions;
+//using System.Linq;
 
-namespace ShoppingCore.Application.Interfaces
-{
-    public interface IDatabaseService
-    {
-        DbSet<Address> Addresses { get; set; }
+//namespace ShoppingCore.Application.Interfaces
+//{
+//    public interface IDatabaseService
+//    {
+//        DbSet<Address> Addresses { get; set; }
 
-        DbSet<Customer> Customers { get; set; }
+//        DbSet<Customer> Customers { get; set; }
 
-        DbSet<Product> Products { get; set; }
+//        DbSet<Product> Products { get; set; }
 
-        DbSet<Category> Categories { get; set; }
+//        DbSet<Category> Categories { get; set; }
 
-        DbSet<ProductCategory> ProductCategories { get; set; }
+//        DbSet<ProductCategory> ProductCategories { get; set; }
 
-        DbSet<Seller> Sellers { get; set; }
+//        DbSet<Seller> Sellers { get; set; }
 
-        DbSet<User> Users { get; set; }
+//        DbSet<User> Users { get; set; }
 
-        void Save();
+//        void Save();
 
-        //IQueryable<T> Include<T>(string[] Properties);
+//        //IQueryable<T> Include<T>(string[] Properties);
 
 
-    }
-}
+//    }
+//}
