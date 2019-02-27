58e4d2b78737c95d224ceb275b4e4c1523ad9c7d
Chaitra <chaitradangat084@gmail.com>
2019-02-27 08:31:10 +0530

    changing namespace.

--- Persistence/ShoppingCore.Persistence/Interfaces/IEfcoreDatabaseService.cs
+++ Persistence/ShoppingCore.Persistence/Interfaces/IEfcoreDatabaseService.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShoppingCore.Persistence.EfCore.Interfaces
+namespace ShoppingCore.Persistence.Interfaces
 {
     public interface IEfcoreDatabaseService
     {
