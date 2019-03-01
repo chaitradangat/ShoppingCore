63108e99f71a7c770a7b223a77dc9f63fc48a90d
Chaitra <chaitradangat084@gmail.com>
2019-03-01 06:40:03 +0530

    Removed factory for CreateUserModel..also removed createusermodel class was was getting spaghetti.

--- Application/ShoppingCore.Application/Users/Commands/CreateUser/Factory/IUserFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShoppingCore.Domain.Users;
-
-namespace ShoppingCore.Application.Users.Commands.CreateUser.Factory
-{
-    public interface IUserFactory
-    {
-        User Create(string username,string password);
-    }
-}
