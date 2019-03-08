5bb204eaaac3e238f1edbd528c1c5782144efc27
Chaitra <chaitradangat084@gmail.com>
2019-03-08 08:21:47 +0530

    interface to fetch all products

---
+++ Application/ShoppingCore.Application/Products/Queries/GetAllProducts/IGetAllProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using ShoppingCore.Application.ApplicationModels;
+
+namespace ShoppingCore.Application.Products.Queries.GetAllProducts
+{
+    public interface IGetAllProducts
+    {
+        IQueryable<ProductModel> Execute();
+    }
+}
