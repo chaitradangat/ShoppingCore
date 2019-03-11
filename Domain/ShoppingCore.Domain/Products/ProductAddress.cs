42c161bfb185b94d2f85706e2018e4a0adf66ef4
Chaitra <chaitradangat084@gmail.com>
2019-03-11 23:38:41 +0530

    added new bounded context address for Product and its configuration

---
+++ Domain/ShoppingCore.Domain/Products/ProductAddress.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingCore.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Domain.Products
+{
+    public class ProductAddress
+    {
+        public int ProductAddressID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int AddressID { get; set; }
+
+        public Product Product { get; set; }
+
+        public Address Address { get; set; }
+
+        public ProductAddress()
+        {
+
+        }
+    }
+}
---
+++ Providers/ShoppingCore.Provider.EfCore/Configurations/Products/ProductAddressConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoppingCore.Domain.Products;
+
+namespace ShoppingCore.Provider.EfCore.Configurations.Products
+{
+    class ProductAddressConfiguration : IEntityTypeConfiguration<ProductAddress>
+    {
+        public void Configure(EntityTypeBuilder<ProductAddress> builder)
+        {
+            builder.HasKey(pa => pa.ProductAddressID);
+
+            builder.HasOne(pa => pa.Address);
+        }
+    }
+}
