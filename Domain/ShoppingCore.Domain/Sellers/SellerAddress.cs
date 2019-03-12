ef9a6b65febd3f40d72fea5c1c397a02b1ffe051
Chaitra <chaitradangat084@gmail.com>
2019-03-12 08:20:50 +0530

    seller address WIP..

---
+++ Domain/ShoppingCore.Domain/Sellers/SellerAddress.cs
@@ -0,0 +1,26 @@
+﻿using ShoppingCore.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Domain.Sellers
+{
+    public class SellerAddress
+    {
+        public int SellerAddressID { get; set; }
+
+        public int AddressID { get; set; }
+
+        public int SellerInformationID { get; set; }
+
+        public int SellerID { get; set; }
+
+
+        public SellerInformation SellerInformation { get; set; }
+
+        public Address Address { get; set; }
+
+
+
+    }
+}
