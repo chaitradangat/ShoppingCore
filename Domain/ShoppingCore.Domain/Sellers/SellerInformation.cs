3c48c2e7ade4b9f9b446082834f42640255cc17b
Chaitra <chaitradangat084@gmail.com>
2019-03-12 08:18:56 +0530

    SellerInformation model to store more depth of seller information

---
+++ Domain/ShoppingCore.Domain/Sellers/SellerInformation.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingCore.Domain.Common;
+using ShoppingCore.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Domain.Sellers
+{
+    public class SellerInformation : IEntity
+    {
+        public int SellerInformationID { get; set; }
+
+        public int SellerID { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public virtual SellerAddress SellerAddress { get; set; }
+
+
+        public SellerInformation()
+        {
+            Users = new HashSet<User>();
+        }
+    }
+}
