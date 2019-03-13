2cdd2bb6fb526fdf8e67cc860dc27faa52ccc45d
Chaitra <chaitradangat084@gmail.com>
2019-03-13 08:22:02 +0530

    added presentation models / view models

---
+++ Presentation/Models/ShoppingCore.Presentation.Models/Products/ProductCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShoppingCore.Presentation.Models.Interfaces;
+
+namespace ShoppingCore.Presentation.Models.Products
+{
+    public class ProductCategoryViewModel : IPresentationModel
+    {
+    }
+}
