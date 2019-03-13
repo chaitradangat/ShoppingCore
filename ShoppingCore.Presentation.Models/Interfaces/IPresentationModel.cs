26f91d16cb3642571cadf3d76702b5903e53ce6d
Chaitra <chaitradangat084@gmail.com>
2019-03-13 08:16:02 +0530

    Presentation Model Interface

---
+++ ShoppingCore.Presentation.Models/Interfaces/IPresentationModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Presentation.Models.Interfaces
+{
+    public interface IPresentationModel
+    {
+    }
+}
