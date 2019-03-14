c88a8f5c77ed0622b5219a8bbcb65bbf5a575a35
Chaitra <chaitradangat084@gmail.com>
2019-03-14 08:18:35 +0530

    get user query application logic

---
+++ Application/ShoppingCore.Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Domain.Common;
+
+namespace ShoppingCore.Application.Users.Queries.GetUser
+{
+    public class GetUserQuery : IGetUserQuery
+    {
+        private readonly IPersistence<IEntity> _persistence;
+
+        public GetUserQuery(IPersistence<IEntity> persistence)
+        {
+            _persistence = persistence;
+        }
+
+        public IAppModel Execute(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAppModel Execute(int UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAppModel Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ Application/ShoppingCore.Application/Users/Queries/GetUser/IGetUserQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ShoppingCore.Application.Interfaces;
+
+
+namespace ShoppingCore.Application.Users.Queries.GetUser
+{
+    public interface IGetUserQuery
+    {
+        IAppModel Execute(string UserName, string Password);
+
+        IAppModel Execute(int UserID);
+
+        IAppModel Execute();
+    }
+}
