422864a27cdb96bb4e082b291e5f33fd494343d9
Chaitra <chaitradangat084@gmail.com>
2019-03-14 08:16:39 +0530

    encryption decryption logic

---
+++ Utilities/ShoppingCore.Utilities/Encryption/Encryption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Utilities.Encryption
+{
+    public class Encryption : IEncryption
+    {
+        public string EncryptString(string stringData)
+        {
+            return stringData;
+        }
+
+        public string DecryptString(string stringData)
+        {
+            return stringData;
+        }
+
+        public Dictionary<int, string> EncryptBulk(Dictionary<int, string> stringDatas)
+        {
+            return stringDatas;
+        }
+    }
+}
---
+++ Utilities/ShoppingCore.Utilities/Encryption/IEncryption.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCore.Utilities.Encryption
+{
+    public interface IEncryption
+    {
+        string EncryptString(string stringData);
+
+        string DecryptString(string stringData);
+
+        Dictionary<int,string> EncryptBulk(Dictionary<int, string> stringDatas);
+    }
+}
