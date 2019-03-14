2f12ef214e533f7b6ba0a5c68c52ab2c4c58e50a
Chaitra <chaitradangat084@gmail.com>
2019-03-14 08:20:05 +0530

    added attributes to userviewmodel w.i.p

--- Presentation/Models/ShoppingCore.Presentation.Models/Users/UserViewModel.cs
+++ Presentation/Models/ShoppingCore.Presentation.Models/Users/UserViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using ShoppingCore.Domain.Common;
 using ShoppingCore.Presentation.Models.Interfaces;
 
 namespace ShoppingCore.Presentation.Models.Users
 {
     public class UserViewModel : IPresentationModel
     {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsAutheticated { get; set; }
+
+        public AutheticationType AutheticationType { get; set; }
+
+        public UserRole UserRole { get; set; }
     }
 }
