e7a2a1a9173770b47969dd8b4eaec750686d697c
Chaitra <chaitradangat084@gmail.com>
2019-03-15 08:00:30 +0530

    experimenting with properties..

--- Presentation/Models/ShoppingCore.Presentation.Models/Users/UserViewModel.cs
+++ Presentation/Models/ShoppingCore.Presentation.Models/Users/UserViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ShoppingCore.Domain.Common;
+using ShoppingCore.Domain.Interfaces;
 using ShoppingCore.Presentation.Models.Interfaces;
 
 namespace ShoppingCore.Presentation.Models.Users
@@ -19,5 +19,35 @@ namespace ShoppingCore.Presentation.Models.Users
         public AutheticationType AutheticationType { get; set; }
 
         public UserRole UserRole { get; set; }
+
+        //testing properties
+        private int propTest;
+
+        public int PropTest
+        {
+            get
+            {
+                if (propTest > 0)
+                {
+                    return propTest;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            private set
+            {
+                propTest = value;
+            }
+
+        }
+
+        public UserViewModel()
+        {
+            PropTest = 10;
+
+            dynamic x = PropTest;
+        }
     }
 }
