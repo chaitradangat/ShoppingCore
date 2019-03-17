bb15c99aa246f38bd9c1585d05fcfa1e8b276d79
Chaitra <chaitradangat084@gmail.com>
2019-03-17 22:12:50 +0530

    added modelmapper for presentation models

---
+++ Presentation/Models/ShoppingCore.Presentation.Models/PresentationModelMapper/ModelMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ShoppingCore.Application;
+using ShoppingCore.Application.ApplicationModels;
+using ShoppingCore.Presentation.Models.Users;
+
+
+namespace ShoppingCore.Presentation.Models.PresentationModelMapper
+{
+    public static class ModelMapper
+    {
+        public static UserViewModel MapToUserViewModel(this UserModel user)
+        {
+            return
+                new UserViewModel
+                {
+                    UserID = user.UserID,
+                    UserName = user.UserName,
+                    Password = user.Password,
+                    AutheticationType = user.AutheticationType,
+                    UserRole = user.UserRole,
+                    IsAutheticated = user.IsAutheticated
+                };
+        }
+
+    }
+}
