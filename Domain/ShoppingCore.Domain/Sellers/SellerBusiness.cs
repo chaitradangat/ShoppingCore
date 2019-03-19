2144533c7cafce9f68ecaa1975cd1d127ca4cf8c
Chaitra <chaitradangat084@gmail.com>
2019-03-19 08:03:58 +0530

    new domain entity SellerBusiness which is owner of group of sellers

---
+++ Domain/ShoppingCore.Domain/Sellers/SellerBusiness.cs
@@ -0,0 +1,27 @@
+﻿using ShoppingCore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Domain.Sellers
+{
+    public class SellerBusiness : IEntity
+    {
+        public int SellerBusinessID { get; set; }
+
+        public string BusinessName { get; set; }
+        /*
+            Todo: add more business specific fields tax,registration,etc
+        */
+        public virtual ICollection<Seller> Sellers { get; set; }
+
+        public virtual ICollection<SellerBusinessAddress> SellerBusinessAddresses { get; set; }
+
+        public SellerBusiness()
+        {
+            Sellers = new HashSet<Seller>();
+
+            SellerBusinessAddresses = new HashSet<SellerBusinessAddress>();
+        }
+    }
+}
---
+++ Domain/ShoppingCore.Domain/Sellers/SellerBusinessAddress.cs
@@ -0,0 +1,20 @@
+﻿using ShoppingCore.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCore.Domain.Sellers
+{
+    public class SellerBusinessAddress : IEntity
+    {
+        public int SellerBusinessAddressID { get; set; }
+
+        public int SellerBusinessID { get; set; }
+
+        public int AddressID { get; set; }
+
+        public virtual SellerBusiness SellerBusiness { get; set; }
+
+        public virtual Address Address { get; set; }
+    }
+}
