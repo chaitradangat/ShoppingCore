69ad0e3f8ed4d3f70366ea0a559b5cdc206bfaee
Chaitra <chaitradangat084@gmail.com>
2019-03-20 08:03:54 +0530

    Sellerbusiness Repository added..

---
+++ Persistence/ShoppingCore.Persistence/EfCore/Sellers/SellerBusinessRepository.cs
@@ -0,0 +1,38 @@
+﻿using ShoppingCore.Application.Interfaces;
+using ShoppingCore.Domain.Interfaces;
+using ShoppingCore.Domain.Sellers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCore.Persistence.EfCore.Sellers
+{
+    public class SellerBusinessRepository : IRepository<SellerBusiness>
+    {
+        public IEntity Add(SellerBusiness entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(SellerBusiness entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity Find(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<SellerBusiness> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity Update(SellerBusiness entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
